f5921abf243918fa471123bccaabc225bbb64bb6
Percy Herold <33342343+Supertroop6@users.noreply.github.com>
2019-08-29 08:59:52 -0500

    Add files via upload

---
+++ ProcessCustomerPrintOrders/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ProcessCustomerPrintOrders
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Batch batch = new Batch();
+            Console.WriteLine("Welcome to the Inner Working Batch Job Application");
+            Console.WriteLine("To execute a batch job type: start batch");
+
+            string command = Console.ReadLine();
+
+            if (command.Trim().ToLower() == "start batch")
+            {
+                Console.Clear();
+                Console.WriteLine("Innerworking Batch Run" + Environment.NewLine);
+                batch.ReadInputFile();
+                batch.ProcessAllJobs();
+                batch.PrintInvoice();
+                Console.WriteLine("Batch ran successfully.");
+            }
+            else
+            {
+                Console.WriteLine("Exiting batch application.");
+            }
+
+            Console.WriteLine("Press enter to close applicaiton.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ TestBatchApplication/BatchTest.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProcessCustomerPrintOrders;
+
+namespace TestBatchApplication
+{
+    [TestClass]
+    public class BatchTest
+    {
+        /// <summary>
+        /// Base margin is 11%. Test base margin.
+        /// </summary>
+        [TestMethod]
+        public void ValidateJobCalculationsValidateStandardMargin()
+        {
+            //Arrange
+            PrintItem item = new PrintItem();
+            Job job = new Job(item);
+
+            item.JobId = 0;
+            item.ChargeSaleTax = true;
+            item.ChargeExtraMargin = false;
+            item.Cost = 100;
+
+            //Act
+            job.ProcessAllPrintItems();
+
+            //Assert margin cost is $11 for a $100
+            Assert.AreEqual(item.MarginCost, 11);
+        }
+        /// <summary>
+        /// Extra margin is 16%. Test extra margin.
+        /// </summary>
+        [TestMethod]
+        public void ValidateJobCalculationsValidateExtraMargin()
+        {
+            //Arrange
+            PrintItem item = new PrintItem();
+            Job job = new Job(item);
+
+            item.JobId = 0;
+            item.ChargeSaleTax = true;
+            item.ChargeExtraMargin = true;
+            item.Cost = 100;
+
+            //Act
+            job.ProcessAllPrintItems();
+
+            //Assert extra margin cost is $16 for a $100 item.
+            Assert.AreEqual(item.MarginCost, 16);
+        }
+        /// <summary>
+        /// Sales Tax rate is 7%. Test sales tax rate.
+        /// </summary>
+        [TestMethod]
+        public void ValidatePrintItemStandardTax()
+        {
+            //Arrange
+            PrintItem item = new PrintItem();
+            Job job = new Job(item);
+
+            item.JobId = 0;
+            item.ChargeSaleTax = true;
+            item.ChargeExtraMargin = true;
+            item.Cost = 100;
+
+            //Act
+            job.ProcessAllPrintItems();
+
+            //Assert sales tax is $7 for a $100 item.
+            Assert.AreEqual(item.SalesTax, 7);
+        }
+        /// <summary>
+        /// If Tax exempted job. Then total tax amount should be $0. Test exempted tax status.
+        /// </summary>
+        [TestMethod]
+        public void ValidatePrintItemExemptTax()
+        {
+            //Arrange
+            PrintItem item = new PrintItem();
+            Job job = new Job(item);
+
+            item.JobId = 0;
+            item.ChargeSaleTax = false;
+            item.ChargeExtraMargin = true;
+            item.Cost = 100;
+
+            //Act
+            job.ProcessAllPrintItems();
+
+            //Assert sale tax is 0 when ChargeSaleTax is false.
+            Assert.AreEqual(item.SalesTax, 0);
+        }
+    }
+}
